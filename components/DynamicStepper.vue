c049ea5473c01b853eef28a46ce42f21c627177e
Dharmik Bhesaniya <dsbhesaniya4811@gmail.com>
2023-09-26 20:45:20 +0530

    update register UI

--- components/DynamicStepper.vue
+++ components/DynamicStepper.vue
@@ -44,16 +44,14 @@ const backProgress = () => {
 </script>
 
 <template>
-  <div class="main-register relative">
+  <div class="main-register relative h-[95%]">
     <div
-      class="register-content flex mb-16 pb-10 items-stretch overflow-scroll h-[34rem]"
+      class="register-content flex overflow-scroll h-[34rem] justify-evenly justify-center lg:items-stretch"
     >
-      <div class="imgdiv register-img lg:basis-1/2 self-start mt-20">
-        <Image
-          src="/images/login-side.jpg"
-          alt="logo"
-          class="w-[30rem] hide-img"
-        />
+      <div
+        class="imgdiv register-img lg:basis-1/2 self-start mt-20 lg:block hidden"
+      >
+        <Image src="/images/login-side.jpg" alt="logo" class="w-[30rem]" />
       </div>
       <div class="compdiv self-start justify-items-start basis-1/2">
         <component
@@ -63,7 +61,7 @@ const backProgress = () => {
       </div>
     </div>
     <div
-      class="register-progress absolute bottom-0 grid grid-flow-row-dense grid-cols-5 grid-rows-1 gap-x-2.5"
+      class="register-progress w-full flex justify-between absolute bottom-0 grid grid-flow-row-dense grid-cols-5 grid-rows-1 gap-x-2.5"
     >
       <q-linear-progress
         v-for="(val, index) in progressBars"
@@ -78,14 +76,14 @@ const backProgress = () => {
     </div>
     <div class="row">
       <q-btn
-        color="primary absolute bottom-8"
+        color="primary absolute bottom-4"
         @click="backProgress"
         :disable="compIndex >= 1 ? false : true"
         >Back</q-btn
       >
       <q-space />
       <q-btn
-        color="primary absolute bottom-8 right-2"
+        color="primary absolute bottom-4 right-2"
         @click="nextProgress"
         v-if="compIndex < components.length"
         :label="step === 5 ? 'Register' : 'Save & Continue'"
@@ -98,27 +96,21 @@ const backProgress = () => {
 .progress-width {
   width: auto;
 }
-.register-progress {
+
+/* .register-progress {
   width: 100%;
   display: flex;
   justify-content: space-between;
-}
+} */
+
+/* .register-content {
+  justify-content: space-evenly;
+} */
+
 .register-content::-webkit-scrollbar {
   display: none;
 }
-.register-content {
-  justify-content: space-evenly;
-}
-
-@media screen and (max-width: 865px) {
-  .register-content {
-    justify-content: center;
-  }
-}
 
 @media screen and (max-width: 1024px) {
-  .hide-img {
-    display: none;
-  }
 }
 </style>
