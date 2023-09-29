a7fc826f2741d6f768b45acea843d8b93350da21
Abhi Patel <abhipatel6788@gmail.com>
2023-09-29 08:54:39 +0530

    add-analytics-page

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,8 @@ useHead({
 definePageMeta({
   middleware: "user-auth",
 });
-// const { gtag, grantConsent, revokeConsent } = useGtag();
-
-// function acceptTracking() {
-//   grantConsent();
-// }
 </script>
 
 <template>
-  <ClientOnly>
-    <h4>Interwave</h4>
-    <!-- <button @click="acceptTracking">Accept Tracking</button> -->
-  </ClientOnly>
+  <h4>Interwave</h4>
 </template>
