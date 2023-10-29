9df632157605cb04a1c63bc39840befcff53771c
Dharmik Bhesaniya <dsbhesaniya4811@gmail.com>
2023-10-29 19:20:31 +0530

    add comment

--- components/sidebar/LaptopMenu.vue
+++ components/sidebar/LaptopMenu.vue
@@ -1,3 +1,38 @@
+<!--^ there are need to add icon as per twitter  -->
+
+<!-- like this -->
+
+<!-- <NuxtLink to="/" class="no-underline text-black">
+      <div
+        class="css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-xyw6el r-o7ynqc r-6416eg"
+      >
+        <div class="css-1dbjc4n">
+          <svg
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-cnnz9e"
+          >
+            <g>
+              <path
+                d="M21.591 7.146L12.52 1.157c-.316-.21-.724-.21-1.04 0l-9.071 5.99c-.26.173-.409.456-.409.757v13.183c0 .502.418.913.929.913h6.638c.511 0 .929-.41.929-.913v-7.075h3.008v7.075c0 .502.418.913.929.913h6.639c.51 0 .928-.41.928-.913V7.904c0-.301-.158-.584-.408-.758zM20 20l-4.5.01.011-7.097c0-.502-.418-.913-.928-.913H9.44c-.511 0-.929.41-.929.913L8.5 20H4V8.773l8.011-5.342L20 8.764z"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <div
+          dir="ltr"
+          class="css-901oao css-1hf3ou5 r-18jsvk2 r-37j5jr r-adyw6z r-16dba41 r-135wba7 r-1joea0r r-88pszg r-bcqeeo r-qvutc0"
+          style="text-overflow: unset"
+        >
+          <span
+            class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
+            style="text-overflow: unset"
+            >Home</span
+          >
+        </div>
+      </div>
+    </NuxtLink> -->
+
 <template>
   <div class="main-menu p-8 h-[40.1rem] font-semibold sticky left-0">
     <NuxtLink to="/" class="no-underline text-black">
