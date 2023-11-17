09a3ed0e37f9a2b5564e85e57dab3ba9e460e390
Dharmik Bhesaniya <dsbhesaniya4811@gmail.com>
2023-11-17 11:04:05 +0530

    add border where layout issue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,55 +1,50 @@
 <template>
-  <div>
-    <nav class="shadow-md bg-white fixed top-0 w-full flex z-[100]">
-      <ul class="flex w-full items-center">
-        <NuxtLink to="/" class="no-underline">
-          <h2 class="md:text-2xl text-xl ml-10 cursor-pointer text-black mr-4 mb-1">
-            Intractify
-          </h2>
-        </NuxtLink>
-        <div class="md:ml-30 lg:ml-48 ml-10">
-          <div class="relative -mx-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              class="w-5 h-5 absolute left-3 bottom-1/2 translate-y-1/2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              ></path>
-            </svg>
-            <input
-              type="search"
-              class="outline-none 
-              input border border-gray-300 lg:w-[20rem] md:w-[15rem] sm:w-[15rem] w-[10rem] rounded-3xl pl-12"
-              placeholder="Search"
-              name="search"
-              id="search"
-            />
-          </div>
-        </div>
-        <div class="md:flex items-center ml-12 text-lg mt-3 hidden">
-          <NuxtLink to="/notification" class="no-underline text-black">
-            <p>Notification</p>
-          </NuxtLink>
-          <NuxtLink to="/notification" class="no-underline text-black ml-10">
-            <p>Blog</p>
-          </NuxtLink>
-        </div>
-        <div class="menu ml-auto cursor-pointer">
-          <Image
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1KOWWKm_0832Msjnhv-MtIxzQY_Wjx72hLLm4x5JODwxSgmGnsyEoPHKP0Wu3zKsuW_c&usqp=CAU"
-            alt="User Profile"
-            class="w-12 h-12 rounded-full mr-8 img-cover"
-          />
-        </div>
-      </ul>
-    </nav>
+  <div class="flex items-center w-full">
+    <NuxtLink to="/" class="no-underline">
+      <h2 class="md:text-2xl text-xl ml-10 cursor-pointer text-black mr-4 mb-1">
+        Intractify
+      </h2>
+    </NuxtLink>
+    <div class="md:ml-30 lg:ml-48 ml-10">
+      <div class="relative -mx-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="w-5 h-5 absolute left-3 bottom-1/2 translate-y-1/2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          ></path>
+        </svg>
+        <input
+          type="search"
+          class="outline-none input border border-gray-300 lg:w-[20rem] md:w-[15rem] sm:w-[15rem] w-[10rem] rounded-3xl pl-12"
+          placeholder="Search"
+          name="search"
+          id="search"
+        />
+      </div>
+    </div>
+    <div class="md:flex items-center ml-12 text-lg mt-3 hidden">
+      <NuxtLink to="/notification" class="no-underline text-black">
+        <p>Notification</p>
+      </NuxtLink>
+      <NuxtLink to="/notification" class="no-underline text-black ml-10">
+        <p>Blog</p>
+      </NuxtLink>
+    </div>
+    <div class="menu ml-auto cursor-pointer">
+      <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1KOWWKm_0832Msjnhv-MtIxzQY_Wjx72hLLm4x5JODwxSgmGnsyEoPHKP0Wu3zKsuW_c&usqp=CAU"
+        alt="User Profile"
+        class="w-12 h-12 rounded-full mr-8 img-cover"
+      />
+    </div>
   </div>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,22 +2,26 @@
   <div class="default-layout">
     <div class="default-content">
       <div v-if="token">
-        <Navbar />
-      </div>
-      <div class="flex items-start justify-center" v-if="token">
-        <div class="md:w-[20%]">
-          <div class="bg-white">
-            <SidebarLaptopMenu class="hidden lg:block" />
-          </div>
-          <div class="w-screen">
-            <SidebarMobileMenu class="block md:hidden" />
-          </div>
-          <div class="bg-white w-[6rem]">
-            <SidebarTabletMenu class="md:block lg:hidden hidden" />
-          </div>
+        <div class="w-[100%] test shadow-md bg-white fixed top-0">
+          <!-- need to re create navbar  -->
+          <Navbar />
+          hello
         </div>
-        <div class="flex items-center justify-evenly lg:px-20 w-[80%]">
-          <slot />
+        <div class="flex items-start justify-center test2">
+          <div class="md:w-[20%]">
+            <div class="bg-white">
+              <SidebarLaptopMenu class="hidden lg:block" />
+            </div>
+            <div class="w-screen">
+              <SidebarMobileMenu class="block md:hidden" />
+            </div>
+            <div class="bg-white w-[6rem]">
+              <SidebarTabletMenu class="md:block lg:hidden hidden" />
+            </div>
+          </div>
+          <div class="flex items-center justify-evenly lg:px-20 w-[80%] test1">
+            <slot class="test1" />
+          </div>
         </div>
       </div>
       <slot v-if="!token" />
@@ -30,6 +34,15 @@ let token = true;
 </script>
 
 <style scoped>
+.test {
+  border: 2px solid red;
+}
+.test1 {
+  border: 2px solid green;
+}
+.test2 {
+  border: 2px solid rgb(0, 60, 128);
+}
 .default-layout {
   width: 100vw;
   height: 100vh;
@@ -40,6 +53,7 @@ let token = true;
   text-align: center;
   overflow: scroll;
   width: 1400px;
+  border: 2px solid black;
 }
 .default-content::-webkit-scrollbar {
   display: none;
